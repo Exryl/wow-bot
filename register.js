b2a03458edd74a7bd50993e9cf6fad544d69c580
Sokko <sokkotkaptam7@gmail.com>
2026-04-15 23:15:16 +0200

    Changed register.js, to have global commands remove process.env.GUILD_ID from routes, slash commands are now avaiable in dms

--- register.js
+++ register.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const { REST, Routes } = require('discord.js');
 
+const everywhere = {
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
 const commands = [
   {
+    ...everywhere,
     name: 'ping',
     description: 'Replies with pong!',
   }
@@ -11,7 +17,7 @@ const commands = [
 const rest = new REST().setToken(process.env.TOKEN);
 
 rest.put(
-  Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+  Routes.applicationGuildCommands(process.env.CLIENT_ID,  process.env.GUILD_ID),
   { body: commands }
 ).then(() => {
   console.log('Commands registered!');
